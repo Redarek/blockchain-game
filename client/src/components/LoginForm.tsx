4ebe67528ce976b64112e88b1e4ac84f8289d7cf
iiVladii <vladislav2407@icloud.com>
2022-10-31 19:04:00 +0300

    connect to metamask

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -4,6 +4,7 @@ import {login} from "../store/reducers/ActionCreators";
 import cl from "../styles/RegistrationForm.module.css";
 import cx from "classnames";
 import {useNavigate} from "react-router-dom";
+import MetaMaskAuth from "./MetamaskConnect/MetaMaskAuth";
 
 declare var window: any
 
@@ -13,76 +14,56 @@ const LoginForm: FC = () => {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
-    if (window.ethereum) {
-        const ethereumButton = document.querySelector('.enableEthereumButton');
-        const showAccount = document.querySelector('.showAccount');
-        //@ts-ignore
-        ethereumButton.addEventListener('click', () => {
-            getAccount();
-        });
-
-        //@ts-ignore
-        async function getAccount() {
-            //@ts-ignore
-            const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-            const account = accounts[0];
-            //@ts-ignore
-            showAccount.innerHTML = account;
-        }
-    } else {
-        alert("install metamask extension!!")
-    }
-
+    const [userAddress, setUserAddress] = useState<string>("");
 
     return (
         <div className={cl.auth}>
-            <form className={cl.auth__form}>
-                {/* start metamask */}
-                <button className="enableEthereumButton">Enable Ethereum</button>
-                <h2>Account: <span className="showAccount"></span></h2>
-                {/* end metamask */}
-                <label htmlFor="login-email" className={cl.auth__label}>
-                    Email
-                </label>
-                <input
-                    className={cx(cl.auth__input, cl.auth__input_email)}
-                    placeholder="Введите email"
-                    name="email"
-                    id="login-email"
-                    onChange={e => setEmail(e.target.value)}
-                    value={email}
-                    type='text'
-                />
-                <label htmlFor="login-password" className={cl.auth__label}>
-                    Пароль
-                </label>
-                <input
-                    className={cx(cl.auth__input, cl.auth__input_password)}
-                    placeholder="Введите пароль"
-                    type="password"
-                    name="password"
-                    id="login-password"
-                    onChange={e => setPassword(e.target.value)}
-                    value={password}
-                />
-                <button className={cx(cl.auth__button, cl.auth__button_login)}
+            {!userAddress
+                ? <MetaMaskAuth onAddressChanged={setUserAddress}/>
+                : <form className={cl.auth__form}>
+                    <label htmlFor="login-email" className={cl.auth__label}>
+                        Email
+                    </label>
+                    <input
+                        className={cx(cl.auth__input, cl.auth__input_email)}
+                        placeholder="Введите email"
+                        name="email"
+                        id="login-email"
+                        onChange={e => setEmail(e.target.value)}
+                        value={email}
+                        type='text'
+                    />
+                    <label htmlFor="login-password" className={cl.auth__label}>
+                        Пароль
+                    </label>
+                    <input
+                        className={cx(cl.auth__input, cl.auth__input_password)}
+                        placeholder="Введите пароль"
+                        type="password"
+                        name="password"
+                        id="login-password"
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
+                    />
+                    <button className={cx(cl.auth__button, cl.auth__button_login)}
+                            onClick={(e) => {
+                                dispatch(login({email: email, password: password}));
+                                e.preventDefault()
+                            }
+                            }
+                    >Войти
+                    </button>
+                    <div
+                        className={cl.auth__text}
                         onClick={(e) => {
-                            dispatch(login({email: email, password: password}));
-                            e.preventDefault()
-                        }
-                        }
-                >Войти
-                </button>
-                <div
-                    className={cl.auth__text}
-                    onClick={(e) => {
-                        navigate("/registration");
-                        e.preventDefault();
-                    }}
-                >
-                    <p className={cl.auth__link}>Регистрация</p>
-                </div>
-            </form>
+                            navigate("/registration");
+                            e.preventDefault();
+                        }}
+                    >
+                        <p className={cl.auth__link}>Регистрация</p>
+                    </div>
+                </form>
+            }
         </div>
     );
 };
---
+++ client/src/components/MetamaskConnect/MetaMaskAuth.tsx
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from "react";
+import styles from "./MetaMaskAuth.module.css";
+
+//@ts-ignore
+async function connect(onConnected) {
+    //@ts-ignore
+    if (!window.ethereum) {
+        alert("Get MetaMask!");
+        return;
+    }
+
+    //@ts-ignore
+    const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+    });
+
+    onConnected(accounts[0]);
+}
+
+//@ts-ignore
+async function checkIfWalletIsConnected(onConnected) {
+    //@ts-ignore
+    if (window.ethereum) {
+        //@ts-ignore
+        const accounts = await window.ethereum.request({
+            method: "eth_accounts",
+        });
+
+        if (accounts.length > 0) {
+            const account = accounts[0];
+            onConnected(account);
+            return;
+        }
+    }
+}
+
+//@ts-ignore
+const MetaMaskAuth = ({onAddressChanged}) => {
+    const [userAddress, setUserAddress] = useState("");
+
+    useEffect(() => {
+        checkIfWalletIsConnected(setUserAddress);
+    }, []);
+
+    useEffect(() => {
+        onAddressChanged(userAddress);
+    }, [userAddress]);
+
+    return (
+        <div>
+            {userAddress
+                ? <div></div>
+                :  <button className={styles.button} onClick={() => connect(setUserAddress)}>
+                    Connect to MetaMask
+                </button>
+            }
+        </div>
+        // <div>
+        //     Connected with
+        //     <span
+        //         className={styles.address}>
+        //         {userAddress.substring(0, 5)}
+        //         …{userAddress.substring(userAddress.length - 4)}
+        //     </span>
+        // </div>
+)
+};
+
+export default MetaMaskAuth;
